aab1488cf935277796027ca5d694ce6615d3be32
mplab <mplab@LCA-06.ele.puc-rio.br>
2017-07-03 08:01:47 -0300

    done

--- bracoRobotico/webInterface/userScript.lua
+++ bracoRobotico/webInterface/userScript.lua
@@ -13,6 +13,11 @@ function user.page(html)
     local page="";
     page = page .. html.time();
     page = page .. html.booleanState("UART","uartChange","PIC","USB");
+    page = page .. html.booleanState("Esquerdo","esquerdo","UP","DOWN");
+    page = page .. html.booleanState("Meio","meio","UP","DOWN");
+    page = page .. html.booleanState("Direito","direito","UP","DOWN");
+    page = page .. html.booleanState("Garra","garra","abrir","fechar");
+    page = page .. html.select("Comando:", {"Movimento1"}, "opts");
     page = page .. html.inText("Comando:", "serialIn", 80)
     return page;
 end
@@ -34,7 +39,46 @@ uartChange = function(value)
                 if (value == 0) then printOn(); else printOff(); end
                 -- Troca os pinos da UART USB <--> PinosAlternativos (D7,D8)
                 uart.alt(value);
-             end
+             end,
+
+direito = function(value)
+    if(value == 0) then
+        uart.write(0, ":p1;end")
+    else 
+        uart.write(0, ":p4;end")
+    end
+end,
+
+meio = function(value)
+    if(value == 0) then
+        uart.write(0, ":p0;end")
+    else 
+        uart.write(0, ":p3;end")
+    end
+end,
+
+esquerdo = function(value)
+    if(value == 0) then
+        uart.write(0, ":p2;end")
+    else 
+        uart.write(0, ":p5;end")
+    end
+end,
+
+garra = function(value)
+    if(value == 0) then
+        uart.write(0, ":g0;end")
+    else 
+        uart.write(0, ":g1;end")
+    end
+end,
+
+opts = function(value)
+    if(value == 1) then
+        uart.write(0, ":p5;:g1;:p2;:g0;:p5;:p4;:p1;:g1;:p4;:p3;:p0;:g0;:p3;:p5;:p2;:g1;:p5;:p3;end")
+    end
+end
+
 }
 
 return user;
